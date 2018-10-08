957f287ecd4b917bb8c9f639af4718ce2b8bab3b
Quentin <vFISHv@gmail.com>
2018-10-08 17:45:16 +0800

    to androidx

--- app/src/main/java/com/sborzenko/materialtoolbarspinner/MainActivity.java
+++ app/src/main/java/com/sborzenko/materialtoolbarspinner/MainActivity.java
@@ -2,11 +2,11 @@ package com.sborzenko.materialtoolbarspinner;
 
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v4.view.MenuItemCompat;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.SearchView;
-import android.support.v7.widget.Toolbar;
+import androidx.annotation.Nullable;
+import androidx.core.view.MenuItemCompat;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.TextView;
